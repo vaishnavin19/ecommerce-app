4097bcce3169ab078b5aff88cf64459edc1dd1ae
vaishnavin19 <65118476+vaishnavin19@users.noreply.github.com>
2025-03-30 21:49:48 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ const productRoutes = require('./routes/products');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+const cors = require("cors");
+app.use(cors({
+  origin: ["https://ecommerce-app-puce-five.vercel.app/", "https://ecommerce-n8b54ifbr-vaishnavis-projects-57a7d752.vercel.app/"],
+  methods: ["GET", "POST"],
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
